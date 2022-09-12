5516415b6705c5c5776901203fd795026d6e0577
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-12 11:47:51 -0300

    feat: finalizando configuração do ambiente server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/ads', (_req, res) => {
-  return res.send('Hello World!');
+  return res.send('Hello World2!');
 });
 
 app.listen(PORT, () => {
