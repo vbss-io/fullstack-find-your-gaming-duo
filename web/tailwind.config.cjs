921fd5fad0339b4bd81f93ad521295d81dc27a7d
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-13 13:32:05 -0300

    feat: Desenvolvendo a página inicial e aplicando Tailwind CSS

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,19 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'nlw-gradient': "linear-gradient(89.86deg, #9572FC 10.08%, #43E7AD 50.94%, #E1D55D 80.57%)",
+        galaxy: "url('/background-nlw.png')",
+        'game-gradient': "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)"
+      }
+    },
   },
   plugins: [],
 }
